bfff976995d05e74c417699ccff3a873aea1259b
André Medeiros <asamedeiros@gmail.com>
2020-05-22 12:01:31 -0300

    new

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          XXX Edit <code>src/App.js</code> and save to reload.
+          AAA Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
